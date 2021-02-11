dd64809004abbe7240218953dc4861a6ec2a270c
Morten Gross <mg@vuffeli.dk>
2021-02-11 23:46:38 +0100

    I'm getting so tired of this shit.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
-const model = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
+const model = [22, 30, 2, 3, 14, 22, 30, 2, 3, 14, 22, 30, 2, 3, 14, 22, 30, 2, 3, 14, 22, 30, 2, 3, 14, 22, 30, 2, 3, 14, 22, 30, 2, 3, 14, 22, 30, 2, 3, 14];
 
 function start() {
     console.log("start");
@@ -11,25 +11,42 @@ function start() {
 
 function loop() {
     console.log("loop");
-    
-    model.forEach((obj) => {
-        const bar = document.createElement("div");
-        bar.classList.add("bar");
-        bar.style.height = obj/32*100 + "%";
-        document.querySelector("#container").append(bar);
-    });
 
-    //setTimeout(loop, 1000);
+    //displayData();
+    setTimeout(displayData, 1000);
 }
 
 function getNumberOfCustomers() {
     console.log("getNumberOfCustomers");
+
+    return Math.floor(Math.random()*32);  
 }
 
 function displayData() {
     console.log("displayData");
+    const bar = document.createElement("div");
+
+    for (let i = 0; i <= 40; i++) {
+        console.log(i);
+        bar.classList.add("bar");
+        bar.style.height = model[i]/32*100 + "%";
+        document.querySelector("#container").append(bar);
+    }
+
+    // model.forEach((obj) => {
+    //     const bar = document.createElement("div");
+    //     bar.classList.add("bar");
+    //     bar.style.height = obj/32*100 + "%";
+    //     document.querySelector("#container").append(bar);
+    // });
+    modifyModel();
 }
 
 function modifyModel() {
     console.log("modifyModel");
+
+    const queueSize = getNumberOfCustomers();
+    // model.push(queueSize);
+    model.splice(39, 1, queueSize);
+    loop();
 }
